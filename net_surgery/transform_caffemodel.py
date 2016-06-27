9adb5f6dd9a644beb6ccb8cbbc4b9b4dcc3557bb
Sheng.Luo <sheng.luo@hpi.de>
2016-06-27 15:41:08 +0200

    update

--- net_surgery/transform_caffemodel.py
+++ net_surgery/transform_caffemodel.py
@@ -12,7 +12,7 @@ import caffe
 # Load the original network and extract the fully connected layers' parameters.
 net = caffe.Net('/home/ls/py-faster-rcnn/net_surgery/original.prototxt','/home/ls/py-faster-rcnn/data/imagenet_models/VGG_CNN_M_1024.v2.caffemodel', caffe.TRAIN)
 params_m = ['conv1']
-params_s = ['conv2','conv3','conv4','conv5', 'rpn_conv/3x3', 'rpn_cls_score', 'rpn_bbox_pred', 'roi_pool5', 'fc6','fc7','cls_score', 'bbox_pred']
+params_s = ['conv2','conv3','conv4','conv5', 'rpn_conv/3x3', 'rpn_cls_score', 'rpn_bbox_pred', 'fc6','fc7','cls_score', 'bbox_pred']
 
 # fc_params = {name: (weights, biases)}
 fc_params_m = {pr: (net.params[pr][0].data, net.params[pr][1].data) for pr in params_m}
@@ -21,7 +21,7 @@ fc_params_s = {pr: (net.params[pr][0].data, net.params[pr][1].data) for pr in pa
 # Load New net
 new_net = caffe.Net('/home/ls/py-faster-rcnn/net_surgery/sdha4.prototxt', caffe.TRAIN)
 new_params_m = ['conv1']
-new_params_s = ['conv2','conv3','conv4','conv5', 'rpn_conv/3x3', 'rpn_cls_score', 'rpn_bbox_pred', 'roi_pool5', 'fc6','fc7','cls_score', 'bbox_pred']
+new_params_s = ['conv2','conv3','conv4','conv5', 'rpn_conv/3x3', 'rpn_cls_score', 'rpn_bbox_pred', 'fc6','fc7','cls_score', 'bbox_pred']
 
 # fc_params = {name: (weights, biases)}
 new_fc_params_m = {pr: (new_net.params[pr][0].data, new_net.params[pr][1].data) for pr in new_params_m} 
